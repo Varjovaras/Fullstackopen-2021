c06651ddee9393b954249916740ab06f49f3e6a3
Varjovaras <78239891+Varjovaras@users.noreply.github.com>
2021-03-29 09:55:50 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,12 +29,12 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <>
-      <p>
+      <>
         Number of exercises{" "}
         {props.parts[0].exercises +
           props.parts[1].exercises +
           props.parts[2].exercises}
-      </p>
+      </>
     </>
   );
 };
