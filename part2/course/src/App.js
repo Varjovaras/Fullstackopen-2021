888d440d83e2f28302b68f4d42c1c1454d124766
Varjovaras <78239891+Varjovaras@users.noreply.github.com>
2021-04-21 18:37:43 +0300

    total component
    123

--- part2/course/src/App.js
+++ part2/course/src/App.js
@@ -1,43 +1,5 @@
 import React from "react";
 
-const Header = ({ header }) => {
-  return <h1>{header}</h1>;
-};
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      <>
-        {parts.map((part) => {
-          return (
-            <Part key={part.id} name={part.name} exercises={part.exercises} />
-          );
-        })}
-      </>
-    </>
-  );
-};
-
-const Part = ({ name, exercises }) => {
-  console.log(name, exercises);
-
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header header={course.name} />
-
-      <Content parts={course.parts} />
-    </>
-  );
-};
-
 const App = () => {
   const course = {
     id: 1,
@@ -63,15 +25,52 @@ const App = () => {
 
   return <Course course={course} />;
 };
-export default App;
 
-// const Total = (props) => {
-//   return (
-//     <>
-//       Number of exercises{" "}
-//       {props.parts[0].exercises +
-//         props.parts[1].exercises +
-//         props.parts[2].exercises}
-//     </>
-//   );
-// };
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header header={course.name} />
+
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
+
+const Header = ({ header }) => {
+  return <h1>{header}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      <>
+        {parts.map((part) => {
+          return (
+            <Part key={part.id} name={part.name} exercises={part.exercises} />
+          );
+        })}
+      </>
+    </>
+  );
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  console.log(parts);
+  const total = parts.reduce((p, s) => p + s.exercises, 0);
+  return (
+    <>
+      <p>total of {total} exercises</p>
+    </>
+  );
+};
+
+export default App;
