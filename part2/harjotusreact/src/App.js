43f5d7d0ab8fa29b2007bc6e777a524a88b2a936
Kristjan Rajaniemi <78239891+Varjovaras@users.noreply.github.com>
2021-04-24 16:15:20 +0300

    added part2b

--- part2/harjotusreact/src/App.js
+++ part2/harjotusreact/src/App.js
@@ -1,22 +1,48 @@
 import React, { useState } from "react";
+import Note from "./components/Note";
 
-const App = () => {
-  const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(false);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   return (
     <div>
-      <button
-        onClick={() =>
-          setCount((currentState) => ({
-            count: currentState.count + 1,
-            count2: currentState.count2,
-          }))
-        }
-      >
-        +
-      </button>
-      <div>count 1: {count}</div>
-      <div>count 2: {count2}</div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
