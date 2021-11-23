688d7ce02bc785f824251f7c44266dcbe6c6e56e
Kristjan Rajaniemi <78239891+Varjovaras@users.noreply.github.com>
2021-11-23 13:23:02 +0200

    commit

--- part4/Blog/tests/blog_api.test.js
+++ part4/Blog/tests/blog_api.test.js
@@ -17,120 +17,120 @@ beforeEach(async () => {
   await blogObject.save();
 });
 
-// test('blogs are json', async () => {
-//   await api
-//     .get('/api/blogs')
-//     .expect(200)
-//     .expect('Content-Type', /application\/json/);
-// });
+test('blogs are json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+});
 
-// test('blog has unique identifier', async () => {
-//   const blog = await api.get('/api/blogs');
-//   expect(blog.body[0].id).toBeDefined();
-// });
+test('blog has unique identifier', async () => {
+  const blog = await api.get('/api/blogs');
+  expect(blog.body[0].id).toBeDefined();
+});
 
-// test('blog is added', async () => {
-//   const newBlog = {
-//     title: 'Jorman seikkailut',
-//     author: 'Markku Markkula',
-//     url: 'http://localhost/api/blog/4',
-//     likes: 55,
-//     username: 'idiot',
-//   };
-//   let response = await api.get('/api/blogs');
+test('blog is added', async () => {
+  const newBlog = {
+    title: 'Jorman seikkailut',
+    author: 'Markku Markkula',
+    url: 'http://localhost/api/blog/4',
+    likes: 55,
+    username: 'idiot',
+  };
+  let response = await api.get('/api/blogs');
 
-//   const blogsLength = response.body.length;
+  const blogsLength = response.body.length;
 
-//   await api
-//     .post('/api/blogs')
-//     .send(newBlog)
-//     .expect(200)
-//     .expect('Content-Type', /application\/json/);
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
 
-//   response = await api.get('/api/blogs');
+  response = await api.get('/api/blogs');
 
-//   const contents = response.body.map((content) => content.title);
+  const contents = response.body.map((content) => content.title);
 
-//   expect(response.body).toHaveLength(blogsLength + 1);
-//   expect(contents).toContain('Jorman seikkailut');
-// });
+  expect(response.body).toHaveLength(blogsLength + 1);
+  expect(contents).toContain('Jorman seikkailut');
+});
 
-// test('likes default to 0', async () => {
-//   const newBlog = {
-//     title: 'Jorman seikkailut',
-//     author: 'Markku Markkula',
-//     url: 'http://localhost/api/blog/4',
-//   };
+test('likes default to 0', async () => {
+  const newBlog = {
+    title: 'Jorman seikkailut',
+    author: 'Markku Markkula',
+    url: 'http://localhost/api/blog/4',
+  };
 
-//   await api
-//     .post('/api/blogs')
-//     .send(newBlog)
-//     .expect(200)
-//     .expect('Content-Type', /application\/json/);
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
 
-//   const blog = await api.get('/api/blogs');
+  const blog = await api.get('/api/blogs');
 
-//   expect(blog.body[2].likes).toBeDefined();
-//   expect(blog.body[2].likes).toBe(0);
-// });
+  expect(blog.body[2].likes).toBeDefined();
+  expect(blog.body[2].likes).toBe(0);
+});
 
-// test('test if title and url missing gives 400', async () => {
-//   const newBlog = {
-//     author: 'Jorma Jormala',
-//   };
+test('test if title and url missing gives 400', async () => {
+  const newBlog = {
+    author: 'Jorma Jormala',
+  };
 
-//   await api.post('/api/blogs').send(newBlog).expect(400);
-// });
+  await api.post('/api/blogs').send(newBlog).expect(400);
+});
 
-// test('deletion of a single blog', async () => {
-//   const response = await api.get('/api/blogs');
-//   const blog = response.body;
+test('deletion of a single blog', async () => {
+  const response = await api.get('/api/blogs');
+  const blog = response.body;
 
-//   await api.delete(`/api/blogs/${blog[0].id}`).expect(204);
-// });
+  await api.delete(`/api/blogs/${blog[0].id}`).expect(204);
+});
 
-// test('update the property likes of a single blog', async () => {
-//   const response = await api.get('/api/blogs');
+test('update the property likes of a single blog', async () => {
+  const response = await api.get('/api/blogs');
 
-//   const blog = response.body;
-//   blog[0].likes = 20;
+  const blog = response.body;
+  blog[0].likes = 20;
 
-//   await api.put(`/api/blogs/${blog[0].id}`).send(blog).expect(204);
-// });
+  await api.put(`/api/blogs/${blog[0].id}`).send(blog).expect(204);
+});
 
-// describe('when there is initially one user at db', () => {
-//   beforeEach(async () => {
-//     await User.deleteMany({});
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
 
-//     const passwordHash = await bcrypt.hash('sekret', 10);
-//     const user = new User({ username: 'root', passwordHash });
+    const passwordHash = await bcrypt.hash('sekret', 10);
+    const user = new User({ username: 'root', passwordHash });
 
-//     await user.save();
-//   });
+    await user.save();
+  });
 
-//   test('creation succeeds with a fresh username', async () => {
-//     const usersAtStart = await listHelper.usersInDb();
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await listHelper.usersInDb();
 
-//     const newUser = {
-//       username: 'mluukkai',
-//       name: 'Matti Luukkainen',
-//       password: 'salainen',
-//     };
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    };
 
-//     await api
-//       .post('/api/users')
-//       .send(newUser)
-//       .expect(200)
-//       .expect('Content-Type', /application\/json/);
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
 
-//     const usersAtEnd = await listHelper.usersInDb();
-//     expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+    const usersAtEnd = await listHelper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
 
-//     const usernames = usersAtEnd.map((u) => u.username);
-//     expect(usernames).toContain(newUser.username);
-//   }),
-//     50000;
-// });
+    const usernames = usersAtEnd.map((u) => u.username);
+    expect(usernames).toContain(newUser.username);
+  }),
+    50000;
+});
 
 describe('addition of a new blog', () => {
   beforeAll(async () => {
---
+++ part5/bloglist-backend/tests/blogs.js
@@ -0,0 +1,24 @@
+const initialBlogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0,
+    username: 'ttune',
+    id: '5e31f5b9886d91753a61271ed',
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+    username: 'adsfadsf',
+    id: '5e31f5b9886d91753a6127143',
+  },
+];
+
+module.exports = initialBlogs;
